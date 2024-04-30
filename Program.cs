bea27bc0e78c320ad89bd77042347da1f8ad6e85
Jacob <jacobespo7@yahoo.com>
2024-04-30 14:29:36 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.NetworkInformation;
+
+namespace App011;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    
+        int x = 5;
+        int y =11;
+
+        int z = Max(x,y);
+        Console.WriteLine($"The maximum of {x} and {y} is {z}.");
+
+    } //Hello World
+
+
+    public static int Max(int a, int b)
+    {
+        if(a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+        
+    }
+
+
+    
+}
